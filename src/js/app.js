e186207014994e139a35c213b94718db1dac348e
Adilson <adilson.vital@usp.br>
2020-04-11 23:31:37 +0200

    aa

--- src/js/app.js
+++ src/js/app.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
   particlesJS('particles-js', {
     "particles": {
       "number": {
-        "value": 100,
+        "value": 120,
         "density": {
           "enable": true,
           "value_area": 800
